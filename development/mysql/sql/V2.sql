e2a1b051f4bffb33fc95d649fcacbb4b2cdca591
asasada <terrywu50@hotmail.com>
2022-04-16 16:41:09 +0900

    record_comment.linked_record_id と record_last_access.user_id、record_last_access.record_idにINDEXを追加

--- development/mysql/sql/V2.sql
+++ development/mysql/sql/V2.sql
@@ -1,1 +1,5 @@
-ALTER TABLE record ADD INDEX createdby_status(created_by, status)
+ALTER TABLE record ADD INDEX createdby_status_index(created_by, status);
+
+ALTER TABLE record_comment ADD INDEX linked_record_id_index(linked_record_id);
+
+ALTER TABLE record_last_access ADD INDEX userid_recordid_index(user_id, record_id);
