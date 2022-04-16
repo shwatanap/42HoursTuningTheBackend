e1a5e5a05b448726b9c11a7ad54e5ef5899959f9
Junichi Sasaki <junichi200123@gmail.com>
2022-04-16 17:29:07 +0900

    record.category_id_application_group_index を追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -3,3 +3,5 @@ ALTER TABLE session ADD INDEX value_idx(value);
 ALTER TABLE group_member ADD INDEX user_id_idx(user_id);
 
 ALTER TABLE category_group ADD INDEX group_id_idx(group_id);
+
+ALTER TABLE record ADD INDEX category_id_application_group_index(category_id, application_group);
